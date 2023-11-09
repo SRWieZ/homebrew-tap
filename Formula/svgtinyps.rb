5870fdd7b4afeae899f3492b2fd698f8fcd512e1
Eser DENIZ <srwiez@gmail.com>
2023-11-09 12:36:39 +0000

    wip

--- Formula/svgtinyps.rb
+++ Formula/svgtinyps.rb
@@ -1,12 +1,13 @@
 class SvgTinyPs < Formula
-  desc "CLI Tool for SVG Tiny P/S (Portable and Secure) conversion and BIMI compliance."
+  desc "CLI Tool for SVG Tiny P/S (Portable and Secure) conversion and BIMI compliance"
   homepage "https://github.com/SRWieZ/svgtinyps-cli"
   url "https://github.com/SRWieZ/svgtinyps-cli/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "your_sha256_here" #TODO: Replace by the sha256 of the archive
+  # TODO: Replace by the sha256 of the archive
+  sha256 "ba09fd07fb71f441bb7c30d55bda0e877093cd41d08306e509d5b4abdd313a4f"
   depends_on "php"
 
   def install
-#   TODO: Replace by the phar version, or even better with the binary version and remove depends_on "php"
+    # TODO: Replace by the phar version, or even better with the binary version and remove depends_on "php"
     bin.install "src/svgtinyps.php" => "svgtinyps"
   end
 end
